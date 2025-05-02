39ff734325b0172fce2e7534a762fa3fbe700920
Moh. Su'aidi <moheedsuaidi@gmail.com>
2025-05-02 23:38:40 +0700

    feat: register

--- src/components/views/Register/useRegister.ts
+++ src/components/views/Register/useRegister.ts
@@ -1,6 +1,31 @@
 import { useState } from "react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { IRegister } from "@/types/Auth";
+import authServices from "@/services/auth";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+
+const registerSchema = yup.object().shape({
+  fullName: yup.string().required("Please enter your fullname"),
+  username: yup.string().required("Please enter your username"),
+  email: yup
+    .string()
+    .email("Please enter a valid email")
+    .required("Please enter your email"),
+  password: yup
+    .string()
+    .min(8, "Minimal 8 characters")
+    .required("Please enter your password"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), ""], "Passwords not match")
+    .required("Please enter your password confirmation"),
+});
 
 const useRegister = () => {
+  const router = useRouter();
   const [visiblePassword, setVisiblePassword] = useState({
     password: false,
     confirmPassword: false,
@@ -13,9 +38,44 @@ const useRegister = () => {
     });
   };
 
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setError,
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+  });
+
+  const registerService = async (payload: IRegister) => {
+    const result = await authServices.register(payload);
+    return result;
+  };
+
+  const { mutate: mutateRegister, isPending: isPendingRegister } = useMutation({
+    mutationFn: registerService,
+    onError(error) {
+      setError("root", {
+        message: error.message,
+      });
+    },
+    onSuccess() {
+      router.push("/auth/register/success");
+      reset();
+    },
+  });
+
+  const handleRegister = (data: IRegister) => mutateRegister(data);
+
   return {
     visiblePassword,
     handleVisiblePassword,
+    control,
+    handleSubmit,
+    handleRegister,
+    isPendingRegister,
+    errors,
   };
 };
 
